8267f6c1a97820eaedd11e8c676e02ee7fdfcdc6
John Matthews <jwmatthews@gmail.com>
2023-10-28 10:52:11 -0400

    Fetch an example with starting point and ending point, Java to Quarkus

---
+++ data/fetch.sh
@@ -0,0 +1,8 @@
+
+pushd .
+# Starting point
+git clone https://github.com/deewhyweb/eap-coolstore-monolith.git coolstuff-javaee
+# Migrated example for Quarkus
+git clone https://github.com/mathianasj/eap-coolstore-monolith.git coolstuff-quarkus
+popd
+
